486cdd3dfbac09b274ca519771a4fbdfe579f5d5
Zumh <17211423+Zumh@users.noreply.github.com>
2023-10-25 16:53:21 -0400

    Update HelloWin.cpp

--- Chapter01/HelloWin/HelloWin.cpp
+++ Chapter01/HelloWin/HelloWin.cpp
@@ -1,5 +1,38 @@
 // HelloWin.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+/*
+The code includes necessary header files: <Windows.h> for Windows API functions and <stdio.h> for standard input/output functions.
+
+The program defines a preprocessor directive PCH_H to ensure that the header file is only included once.
+
+In the main function:
+
+It declares a variable si of type SYSTEM_INFO to hold system information.
+Calls the GetNativeSystemInfo function to retrieve system information and stores it in the si variable.
+It uses printf to display various pieces of system information:
+
+Number of Logical Processors.
+Page size in bytes.
+Processor Mask in hexadecimal format.
+Minimum process address.
+Maximum process address.
+Finally, the program returns 0 to indicate that it has executed successfully.
+
+In summary, this program provides a basic demonstration of accessing and printing system information using the Windows API.
+*/
+
+/*
+PROCESS MASK
+
+The "Processor Mask" is a concept in computing that involves the use of binary digits to represent which processors or cores in a multi-core system are available for use. 
+It is a bit mask where each bit represents the availability of a specific processor.
+
+For example, in a system with 4 processors, the processor mask might look like this in binary: 1111, which in hexadecimal notation would be 0xF. 
+This means that all four processors are available for use.
+
+In the provided code, si.dwActiveProcessorMask is a member of the SYSTEM_INFO structure returned by the GetNativeSystemInfo function. 
+It provides information about which processors are currently configured for use in the system. This information can be used to optimize tasks in a multi-threaded application by distributing workloads across available processors.
+*/
 
 #include "pch.h"
 
